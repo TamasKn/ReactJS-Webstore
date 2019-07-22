8628f65a83fd798bd612470781ebc45d8c9a034f
TamasKn <36684048+TamasKn@users.noreply.github.com>
2019-07-22 19:07:59 +0200

    Temporary local storage cart items

--- src/Redux/RootReducer.js
+++ src/Redux/RootReducer.js
@@ -2,8 +2,18 @@
 import {combineReducers} from 'redux'
 import UserReducer from './UserReducer/UserReducer'
 import CartReducer from './CartReducer/CartReducer'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
     user: UserReducer,
     cart: CartReducer
 })
+
+export default persistReducer(persistConfig, rootReducer)
--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -3,8 +3,10 @@
 import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import rootReducer from './RootReducer'
+import { persistStore } from 'redux-persist'
 
 const middlewares = [logger]
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
+export const store = createStore(rootReducer, applyMiddleware(...middlewares))
+export const persistor = persistStore(store)
 
-export default store
+export default { store, persistor }
